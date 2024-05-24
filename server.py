09f847fb6f3b8c5174307298bdf2462ecc649299
DAVINCI\notok <harshitjawla123@gmail.com>
2024-05-24 17:45:52 +0530

    Second commit

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import requests
 import re, time, json
 from flight_details_api import get_flight_details
 from flight_safe_api import get_flight_safe
+from flight_nearest_airport import get_nearest_airports
 
 app = Flask(__name__)
 
@@ -13,25 +14,47 @@ def getflightDetails(flight=None):
         flight = request.args.get("flight", "6E17")
     return get_flight_details(flight)
 
+def getAllFlightInfo(lat, lon):
+    all_flight_info = get_flight_safe(lat, lon).get_json()
+    is_safe = all_flight_info.get("is_safe", False)
+    # is_safe = True
+    if is_safe == False:
+        nearest_airports = get_nearest_airports(lat, lon).get_json()
+        all_flight_info["nearest_airports"] = nearest_airports
+    return jsonify(all_flight_info)
 
 @app.route("/flight-safe")
 def getFlightSafe():
     lat = request.args.get("lat", "0")
     lon = request.args.get("lon", "0")
     if lat != "0" and lon != "0":
-        return get_flight_safe(lat, lon)
+        return getAllFlightInfo(lat, lon)
 
     flight = request.args.get("flight", "6E17")
     flight_details = get_flight_details(flight).get_json()
-    if flight_details.get("success", "false") == "false":
-        return flight_details
 
     positions = flight_details.get("data", {}).get("positions", [])
     if len(positions) > 0:
         lat = positions[0].get("lat", "0")
         lon = positions[0].get("lon", "0")
-    return get_flight_safe(lat, lon)
+        
+    return getAllFlightInfo(lat, lon)
 
+# @app.route("/nearest-airports")
+# def getNearestAirports():
+#     lat = request.args.get("lat", "0")
+#     lon = request.args.get("lon", "0")
+#     if lat != "0" and lon != "0":
+#         return get_nearest_airports(lat, lon)
+
+#     flight = request.args.get("flight", "6E17")
+#     flight_details = get_flight_details(flight).get_json()
+
+#     positions = flight_details.get("data", {}).get("positions", [])
+#     if len(positions) > 0:
+#         lat = positions[0].get("lat", "0")
+#         lon = positions[0].get("lon", "0")
+#     return get_nearest_airports(lat, lon)
 
 if __name__ == "__main__":
     app.run(debug=True)
